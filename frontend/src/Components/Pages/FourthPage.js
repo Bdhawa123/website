eaff85201560490683784d199038e71f71cd5841
Binay Dhawa <binaydhawa@outlook.com>
2021-01-30 20:14:58 +1100

    gallery ready

--- frontend/src/Components/Pages/FourthPage.js
+++ frontend/src/Components/Pages/FourthPage.js
@@ -1,9 +1,10 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import axios from "axios";
 import "../../styles/FourthPage.css";
 import { CloudinaryContext, Transformation, Image } from "cloudinary-react";
 
 const FourthPage = () => {
+  const mouseEnterRef = useRef(null);
   const [Project, setProject] = useState([]);
 
   useEffect(() => {
@@ -12,22 +13,27 @@ const FourthPage = () => {
         "http://res.cloudinary.com/classic-euphemist/image/list/gallery.json"
       )
       .then((res) => {
-        console.log(res.data.resources);
+        // console.log(res.data.resources);
         setProject(res.data.resources);
       });
   }, []);
 
+  let mouseEnter = () => {
+    mouseEnterRef.current.addEventListener("mouseleave", () => {
+      console.log("Mouse Leave");
+    });
+  };
+
   return (
     <div className="main_container">
-      <div className="Title">PHOTOGRAPHY</div>
-      <div className="glass_pane">
+      <div
+        className="gallery_container"
+        onMouseEnter={console.log("Mouse Enter")}
+        onMouseLeave={console.log("Mouse Leave")}
+      >
         <div className="gallery">
-          <CloudinaryContext
-            cloudName="classic-euphemist"
-            className="photo_gallery"
-          >
+          <CloudinaryContext className="photo_gallery">
             {Project.map((data) => {
-              console.log(data);
               return (
                 <div key={`${data.public_id}.jpg `}>
                   <img
@@ -40,6 +46,7 @@ const FourthPage = () => {
           </CloudinaryContext>
         </div>
       </div>
+      <div className="Title">PHOTOGRAPHY</div>
     </div>
   );
 };
